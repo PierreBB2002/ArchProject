a75c8d60fd0555663cc57a56ad714a0144a5d8f2
ChristinaNSaba <155658632+ChristinaNSaba@users.noreply.github.com>
2024-01-09 18:35:44 +0200

    Add files via upload

--- InstReg.v
+++ InstReg.v
@@ -2,79 +2,51 @@ module IR(
 	input [31:0] PC;
 	input [31:0] inst;
 	output reg [31:26] opcode;
-	output reg [3:0] inst_rs1 , inst_rs2 , inst_rd; 
+	output reg [3:0] inst_rs1, inst_rs2, inst_rd; 
 	output reg [15:0] imm_16; 
-	output reg [25:0] imm_26;
-	output reg [1:0] mode;
-);				
+	output reg [31:0] imm_26;
+	output reg [1:0] mode
+);
 
-parameter  [3:0] R_Type_Bits;
-parameter [5:0] I_Type_Bits;  
+parameter [3:0] R_Type_Bits = 4'b0000;
+parameter [5:0] I_Type_Bits = 6'b000000;
 
-opcode = inst[31:26];
+always @(inst)
+begin 
+	R_Type_Bits = inst[31:28]; // first 4 bits of the opcode
+	I_Type_Bits = inst[31:26]; // first 6 bits of the opcode
 
-	always @(inst)
+	opcode = inst[31:26];
+
+	if (R_Type_Bits == 4'b0000 && (I_Type_Bits != 6'b000011)) // R-Type
 	begin 
-		R_Type_Bits = inst[31:28];
-		I_Types_Bits = inst [31:26];
-		if (R_Type_Bits == 4'b0000)	 //R-Type
-			begin 
-			inst_rd = inst[25:22]; 
-			inst_rs1 = inst[21:18]; 
-			inst_rs2 = inst[17:14]; 
-		end   
-		
-		else if (R_Type_Bits == 4'b0001 || R_Type_Bits == 4'b0010 || R_Type_Bits == 4'b0011 && I_Type_Bits != 6'b001111) // I_Type
-			begin 
-			inst_rd = inst[25:22]; 
-			inst_rs1 = inst[21:18]; 
-			imm_16 = inst[17:2]; 
-			mode = inst[1:0];
-		end 
-		
-		else if (I_Type_Bits == 6'b001111 || I_Type_Bits == 6'b010000) //J_Type --> JMP
-			begin 
-			imm_24 = {PC[31:26],inst[25:0]}; 
-		end		
-		
-		else if (I_Type_Bits == 6'010001) //J_Type --> CALL
-			begin 
-			 imm_24 = {PC[31:26],inst[25:0]};
-			 
-			 
-		     end	
-		else if (I_Type_Bits == 6'010010) //J_Type --> JR 
-			begin
-			  inst_rd = inst[25:22];
-		end	
-		else if (I_Type_Bits == 6'010001) //J_Type --> return
-			begin 
-			
-		end	   
-		else if (I_Type_Bits == 6'010011) //J_Type --> CALL.R
-			begin 
-			
-		end	
-		
-		else if (I_Type_Bits == 6'010101 || I_Type_Bits == 6'010111 )	//S-Type .M
-		    begin
-				inst_rd = inst[25:22];
-				inst_rs1 = inst[21:18];
-		    end
-		else if (I_Type_Bits == 6'010100 || I_Type_Bits == 6'010110 )	//S-Type .1
-			 begin
-				inst_rd = inst[25:22];
-		    end	
-
-		
-	    else begin 
-		   	inst_rs1 = inst[26:22] ; 
-			inst_rd = inst[21:17] ; 
-			inst_rs2 = inst[16:12] ; 
-			inst_SA = inst[11:7]; 
-		end
+		inst_rd = inst[25:22]; 
+		inst_rs1 = inst[21:18]; 
+		inst_rs2 = inst[17:14]; 
+	end   
+	
+	else if ((R_Type_Bits == 4'b0001) || (R_Type_Bits == 4'b0010) || (I_Type_Bits == 6'b000011)) // I_Type
+	begin 
+		inst_rd = inst[25:22]; 
+		inst_rs1 = inst[21:18]; 
+		imm_16 = inst[17:2]; 
+		mode = inst[1:0];
+	end 
+	
+	else if (I_Type_Bits == 6'b001100) // J_Type --> JMP
+	begin 
+		imm_26 = {PC[31:26], inst[25:0]}; 
+	end		
+	
+	else if (I_Type_Bits == 6'b001101) // J_Type --> CALL
+	begin 
+		imm_26 = {PC[31:26], inst[25:0]};
 	end	
-	
-	
-	
+	else if ((I_Type_Bits == 6'b001111) || (I_Type_Bits == 6'b010000)) // S-Type PUSH or POP
+	begin
+		inst_rd = inst[25:22];
+	end
+				
+end
+
 endmodule
